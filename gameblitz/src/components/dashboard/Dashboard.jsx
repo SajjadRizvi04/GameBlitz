57e3b35cc158691a68b2dd54d036341235dae196
Justin W Chada <chadajustin@gmail.com>
2026-04-03 19:08:06 +0530

    Justin - cleared the LandingPage test code from Dashboard.jsx

--- gameblitz/src/components/dashboard/Dashboard.jsx
+++ gameblitz/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,12 @@
 import DashboardSidebar from "./sidebar/Sidebar";
 import { Outlet } from 'react-router'
-import LandingNoTeam from "./no-team/LandingNoTeam";
 export function Dashboard(){
 
     return(
         <>
             <section className="flex dashboard-template">
-                {/* <DashboardSidebar /> */}
-                {/* <Outlet /> */}
-                <LandingNoTeam />
+                <DashboardSidebar />
+                <Outlet />
             </section>
         </>
     )
