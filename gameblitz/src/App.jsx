41fbcc30f91c79c63c03102623e64af6788d3478
Sajjad <rizvisajjad80@gmail.com>
2026-04-03 13:29:57 +0530

    Done with map section of home page

--- gameblitz/src/App.jsx
+++ gameblitz/src/App.jsx
@@ -1,12 +1,13 @@
-import { Routes } from 'react-router';
-import './App.css'
+import { Route, Routes } from 'react-router';
+import './App.css';
+import Home from './components/home/Home';
 
 function App() {
 
   return (
     
       <Routes>
-        
+        <Route path='/home' element={<Home/>}></Route>
       </Routes>
   );
 }
